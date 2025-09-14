2e8fe1c0aeef58ca9d061358565924caf3ef34a2
rausazy <rausazy@gmail.com>
2025-09-14 22:35:46 +0800

    Stocks Auth
    inayos lang auth ng stocks

--- routes/web.php
+++ routes/web.php
@@ -6,56 +6,46 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\StocksController;
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('home'); 
-    });
 
-    Route::get('/products', function () {
-        return view('products'); 
-    });
+// ----- GUEST ROUTES -----
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
 
-    Route::get('/services', function () {
-        return view('services'); 
-    });
-
-    Route::get('/profile', function () {
-        return view('profile'); 
-    });
-
-    Route::get('/stocks', function () {
-        return view('stocks'); 
-    });
+    Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('register.form');
+    Route::post('/register', [LoginController::class, 'register'])->name('register');
 });
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
-Route::post('/login', [LoginController::class, 'login'])->name('login');
-
-Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('register.form');
-Route::post('/register', [LoginController::class, 'register'])->name('register');
-
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
+// ----- AUTH ROUTES -----
 Route::middleware('auth')->group(function () {
+
+    // Home / Dashboard
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
+
+    // Profile
+    Route::get('/profile', function () {
+        return view('profile');
+    })->name('profile');
+
+    // Stocks
+    Route::get('/stocks', [StocksController::class, 'index'])->name('stocks.stocks');
+
+    // Products
     Route::get('/products', [ProductsController::class, 'index'])->name('products.products');
     Route::get('/products/create', [ProductsController::class, 'create'])->name('products.create');
     Route::post('/products', [ProductsController::class, 'store'])->name('products.store');
-    Route::delete('/products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy')->middleware('auth');
+    Route::delete('/products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
     Route::get('/products/{url}', [ProductsController::class, 'show'])->name('products.show');
-});
 
-Route::middleware('auth')->group(function () {
+    // Services
     Route::get('/services', [ServiceController::class, 'index'])->name('services.services');
     Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create');
     Route::post('/services', [ServiceController::class, 'store'])->name('services.store');
-    Route::delete('/services/{service}', [ServiceController::class, 'destroy'])->name('services.destroy')->middleware('auth');
+    Route::delete('/services/{service}', [ServiceController::class, 'destroy'])->name('services.destroy');
     Route::get('/services/{url}', [ServiceController::class, 'show'])->name('services.show');
+
+    // Logout
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
-
-    
-Route::get('/stocks', [StocksController::class, 'index'])->name('stocks.stocks');
-
-
-
-
-
